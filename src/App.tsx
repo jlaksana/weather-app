f85f1fe4efd072a1448bd9419f8f7febdb1795f4
jonathanalaksana <jonathanalaksana@gmail.com>
2022-08-09 18:59:03 -0700

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 
 function App() {
+  // const [location, setLocation] = useState("San Luis Obispo");
+  // const [data, setData] = useState({});
+
+  // const urlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=01cef1176e62c3bf636fe8275cc2382d`;
+  // const urlHourly = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${location}&units=imperial&appid=01cef1176e62c3bf636fe8275cc2382d`;
+  // const urlDaily = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&cnt=5&units=imperial&appid=01cef1176e62c3bf636fe8275cc2382d`;
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>Sacramento</h1>
+        <h3>89 F</h3>
       </header>
     </div>
   );
